616d39c9ebc36159be345f8816135e19d5776a37
Robert Kozak <rkozak@nowcom.com>
2017-09-08 12:40:41 -0700

    Added logging

--- 4.0/run.sh
+++ 4.0/run.sh
@@ -3,4 +3,7 @@ if [ ! -z "$CONTENT_VCL" ]; then
 	echo -e "$CONTENT_VCL" > /etc/varnish/default.vcl
 	CONTENT="-f /etc/varnish/default.vcl"
 fi
-exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
+(sleep 10; /etc/init.d/varnishncsa start) & 
+exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS &
+tail -f /var/log/varnish/varnishncsa.log
+

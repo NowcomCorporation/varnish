7284e0763e74dea643ac0de59c2f58e434e328fd
Robert Kozak <rkozak@nowcom.com>
2017-09-08 14:50:33 -0700

    Updated Dockerfile

--- 4.0/run.sh
+++ 4.0/run.sh
@@ -5,7 +5,7 @@ if [ ! -z "$CONTENT_VCL" ]; then
 fi
 
 touch /var/log/varnish/varnishncsa.log
-exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS &
+exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
 
 #(sleep 10; /etc/init.d/varnishncsa start) &
 #tail -f /var/log/varnish/varnishncsa.log &

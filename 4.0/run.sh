49efeb31e5113e533a42f988b75ea9158c57c5ca
Robert Kozak <rkozak@nowcom.com>
2017-10-08 13:19:12 -0700

    changed start order again

--- 4.0/run.sh
+++ 4.0/run.sh
@@ -4,13 +4,13 @@ if [ ! -z "$CONTENT_VCL" ]; then
 	CONTENT="-f /etc/varnish/default.vcl"
 fi
 
+echo "Start varnishd"
+exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
+
 # Dashboard for Varnish Agent2
 if [ "$ENABLE_DASHBOARD" == "True" ]
 then
   echo "start varnish-agent"
-  varnish-agent -H /var/www/dashboard/
+  varnish-agent -H /var/www/dashboard/ -n $VARNISH_NAME -p /etc/varnish/ -z 127.0.0.1:80 -d -v
 fi
-
-echo "Start varnishd"
-exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
 

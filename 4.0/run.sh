de4c81a01f149122aed138b50ea87fcf44a7c788
Robert Kozak <rkozak@nowcom.com>
2017-10-08 14:01:41 -0700

    testing: always start varnish agent

--- 4.0/run.sh
+++ 4.0/run.sh
@@ -5,7 +5,7 @@ if [ ! -z "$CONTENT_VCL" ]; then
 fi
 
 echo "Start varnishd"
-/usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS &
+/usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret $VARNISH_OPTS
 
 # Dashboard for Varnish Agent2
 #if [ "$ENABLE_DASHBOARD" == "True" ]

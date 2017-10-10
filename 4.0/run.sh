a803ea807aa8e580f74b2efc428675517f737c5b
Robert Kozak <rkozak@nowcom.com>
2017-10-10 11:42:34 -0700

    added varnishlog

--- 4.0/run.sh
+++ 4.0/run.sh
@@ -6,15 +6,16 @@ if [ ! -z "$CONTENT_VCL" ]; then
 fi
 
 echo "Start varnishd"
-  
+exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS &
+
 # Dashboard for Varnish Agent2
 if [ "$ENABLE_DASHBOARD" == "True" ]
 then
-  exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS &
-  
+  # wait for varnishd to startup
   sleep 20
+  
   echo "start varnish-agent"
-  varnish-agent -H /var/www/dashboard/ -c 8888 -n $VARNISH_NAME -p /etc/varnish/ -z 127.0.0.1:80 -d -v
-else
-  exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
+  varnish-agent -H /var/www/dashboard/ -c 8888 -n $VARNISH_NAME -p /etc/varnish/ -z 127.0.0.1:80 -v
 fi
+
+varnishlog

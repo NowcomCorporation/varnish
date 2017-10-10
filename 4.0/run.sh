221ac3ec7595c782d4dd007700558e16cb856f54
Robert Kozak <rkozak@nowcom.com>
2017-10-10 11:03:11 -0700

    added sleep

--- 4.0/run.sh
+++ 4.0/run.sh
@@ -11,6 +11,8 @@ echo "Start varnishd"
 if [ "$ENABLE_DASHBOARD" == "True" ]
 then
   exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS &
+  
+  sleep 20
   echo "start varnish-agent"
   varnish-agent -H /var/www/dashboard/ -c 8888 -n $VARNISH_NAME -p /etc/varnish/ -z 127.0.0.1:80 -d -v
 else

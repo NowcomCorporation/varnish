b843018f008c0e4a3c5facf202983337bf65bad9
Robert Kozak <rkozak@nowcom.com>
2017-10-10 09:48:13 -0700

    run varnishd forked so it runs in background

--- 4.0/run.sh
+++ 4.0/run.sh
@@ -6,12 +6,12 @@ if [ ! -z "$CONTENT_VCL" ]; then
 fi
 
 echo "Start varnishd"
-exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
+exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret $VARNISH_OPTS
 
 # Dashboard for Varnish Agent2
 #if [ "$ENABLE_DASHBOARD" == "True" ]
 #then
   echo "start varnish-agent"
-  varnish-agent -H /var/www/dashboard/ -n $VARNISH_NAME -p /etc/varnish/ -z 127.0.0.1:80 -v
+  varnish-agent -H /var/www/dashboard/ -n $VARNISH_NAME -p /etc/varnish/ -z 127.0.0.1:80 -d -v
 #fi
 

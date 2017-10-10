73ab3e422b2ee75263d968ac5fc1f6246dacefac
Robert Kozak <rkozak@nowcom.com>
2017-10-10 10:46:29 -0700

    Update run.sh

--- 4.0/run.sh
+++ 4.0/run.sh
@@ -5,15 +5,14 @@ if [ ! -z "$CONTENT_VCL" ]; then
 	CONTENT="-f /etc/varnish/default.vcl"
 fi
 
-# Dashboard for Varnish Agent2
-#if [ "$ENABLE_DASHBOARD" == "True" ]
-#then
-  echo "start varnish-agent"
-  varnish-agent -H /var/www/dashboard/ -c 8888 -n $VARNISH_NAME -p /etc/varnish/ -z 127.0.0.1:80 -v
-#fi
-
 echo "Start varnishd"
-exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
-
-
-
+  
+# Dashboard for Varnish Agent2
+if [ "$ENABLE_DASHBOARD" == "True" ]
+then
+  exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS &
+  echo "start varnish-agent"
+  varnish-agent -H /var/www/dashboard/ -c 8888 -n $VARNISH_NAME -p /etc/varnish/ -z 127.0.0.1:80 -d -v
+else
+  exec /usr/sbin/varnishd -a :$LISTEN_PORT $CONTENT -n $VARNISH_NAME -s $CACHE -S /etc/varnish/secret -F $VARNISH_OPTS
+fi
